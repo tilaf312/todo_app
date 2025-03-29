a4f2d4c86395c611c80f51e4e4983bf1c1adba54
maxim <tilaf312@mail.ru>
2025-03-29 21:57:13 +0300

    add delete endpoint

--- app/models/task.py
+++ app/models/task.py
@@ -8,7 +8,7 @@ from app.models.user import User
 class Task(Base):
     __tablename__ = "tasks"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, unique=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str] = mapped_column(String(250))
     registry_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
--- app/routers/task.py
+++ app/routers/task.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -34,3 +34,17 @@ def get_tasks(
         current_user: User = Depends(get_current_user)
 ):
     return db.query(Task).filter(Task.user_id == current_user.id).all()
+
+
+@router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(
+    task_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    task = db.query(Task).filter(Task.id == task_id, Task.user_id == current_user.id).first()
+    if task is None:
+        raise HTTPException(status_code=404, detail="Задача не найдена")
+    db.delete(task)
+    db.commit()
+    return None
